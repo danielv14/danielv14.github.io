90a2d16c38fc89298836d6bb60b361c5c0319057
Daniel <daniel.vernberg@gmail.com>
2017-01-27 18:53:21 +0100

    gulp setup for concat script files and sourcemaps.
    Exclude other js-files from jekyll build

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 const gulp = require('gulp');
 const image = require('gulp-image');
 const shell = require('gulp-shell');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
 
-
+// Path object containing asset paths
 const paths = {
   'img': {
     'base': 'assets/img/*'
-  }
+  },
+  'js': ['assets/javascript/jquery-3.1.1.min.js', 'assets/javascript/main.js']
 };
+
+// Concat .js files and create sourcemaps
+gulp.task('scripts:concat', function() {
+  return gulp.src(paths.js)
+  .pipe(sourcemaps.init())
+    .pipe(concat('app.js'))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('./assets/javascript/'));
+});
 
 // Task to compress all images inside asset folder
 gulp.task('img:compress', () => {
@@ -25,3 +37,14 @@ gulp.task('serve', shell.task([
 gulp.task('serve:drafts', shell.task([
   "bundle exec jekyll serve --incremental --drafts"
 ]));
+
+
+// Gulp watching
+gulp.task('watch', () => {
+  // concat js files
+  gulp.watch(paths.js, ['scripts:concat']);
+
+});
+
+// Default task triggers gulp watch
+gulp.task('default', ['watch']);
